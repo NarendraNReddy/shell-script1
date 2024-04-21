0ca78fddd859dfdbc6c9f8361bd91c0c9598383b
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 19:58:43 -0700

    first commit

--- 06-array.sh
+++ 06-array.sh
@@ -4,4 +4,4 @@ movies=("RRR" "RANGA" "RAMARAJ BANIAN")
 
 echo "First movie name is:${movies[0]}"
 echo "Second movie name is:${movies[1]}"
-echo "All movie names are :${movies[@]}
+echo "All movie names are :${movies[@]}"
