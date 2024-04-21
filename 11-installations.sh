71240c52667532ff23a3ba432cf792c31151a53a
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 21:33:35 -0700

    first commit

--- 11-installations.sh
+++ 11-installations.sh
@@ -1,7 +1,8 @@
 #!/usr/bin
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-echo $SCRIPT_NAME
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+echo $LOGFILE
 VALIDATE()
 {
     if [ $? -ne 0 ];
