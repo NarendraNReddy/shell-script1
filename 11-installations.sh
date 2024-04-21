6b4cda58d97623f578c66aceb2951b3eeeb43aa4
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 21:44:04 -0700

    first commit

--- 11-installations.sh
+++ 11-installations.sh
@@ -3,15 +3,21 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-echo $LOGFILE
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+
 VALIDATE()
 {
     if [ $? -ne 0 ];
     then
-        echo "$2 is ... FAILURE"
+        echo -e "$2 is ... $R FAILURE $N"
         exit 1 
     else 
-        echo "$2 is .... SUCCESS"    
+        echo -e "$2 is ....$G SUCCESS $N"    
     fi    
 }
 
