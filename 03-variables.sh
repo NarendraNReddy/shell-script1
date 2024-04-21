fe258ae0fa5018f539b76972d27cd3353f6402a2
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 19:47:43 -0700

    first commit

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,10 @@
 #!/usr/bin
 
-PERSON1=RAMESH
-PERSON2=SURESH
+#PERSON1=RAMESH
+#PERSON2=SURESH
+
+PERSON1=SACHIN
+PERSON2=RAAGA
 
 echo "$PERSON1:: Hi $PERSON2,how are you"
 echo "$PERSON2:: Hi $PERSON1,i m fine. How are you"
