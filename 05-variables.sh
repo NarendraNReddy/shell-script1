da4841d628f81be8b613055782925b1099106bab
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 19:55:48 -0700

    first commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,10 @@
 #!/usr/bin
 echo "Enter user name"
-read USERNAME
+#read USERNAME
+read -s USERNAME
 
 echo "Enter password"
-read PASSWORD
+#read PASSWORD
+read -s PASSWORD
 
 echo  "User name is $USERNAME,pasword :$PASSWORD"
