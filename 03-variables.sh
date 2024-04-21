891bafbbe741c6b55a2e6c4f17d27c6aa6531f97
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 19:46:51 -0700

    first commit

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/usr/bin
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1:: Hi $PERSON2,how are you"
+echo "$PERSON2:: Hi $PERSON1,i m fine. How are you"
+echo "$PERSON1:: I am fine. How about your office"
+echo "$PERSON2:: Good. Planning to learn devops"
